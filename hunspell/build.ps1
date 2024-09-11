d40cc3a6fa725d5d1328693f8e194ec3b732805e
Jon Thysell <thysell@gmail.com>
2024-09-10 22:11:36 -0700

    Update base.aff with WORDCHARS, MAP, ICONV, add calculated TRY to build

--- hunspell/build.ps1
+++ hunspell/build.ps1
@@ -43,6 +43,27 @@ try
     $HawAffFile = Join-Path $HunspellOutputDir "haw.aff"
     Write-Host Creating $HawAffFile...
     Copy-Item -Path (Join-Path $HunspellInputDir "base.aff") -Destination $HawAffFile -Force
+
+    Write-Host Calculating character histogram...
+    $CharHistogram = @{}
+    $ValidChars = "aāeēiīoōuūhklmnpwʻ"
+    $ValidChars.ToCharArray() | ForEach-Object { $CharHistogram.Add($_.ToString(), 0) }
+    $FinalWords | ForEach-Object {
+        $_.ToCharArray() | ForEach-Object {
+            $key = [Char]::ToLower($_).ToString()
+            if ($ValidChars.Contains($key)) {
+                $CharHistogram[$key]++
+            }
+        }
+    }
+    $TryChars = ""
+    $CharHistogram.GetEnumerator() | Sort-Object { $_.Value } -Descending | ForEach-Object { $TryChars += $_.Key }
+
+    $TryDirective = "TRY $TryChars" + $TryChars.ToUpper().Replace("ʻ", "") + "ʻ"
+    Write-Host "Adding TRY directive to $HawAffFile..."
+    "`r`n$TryDirective" | Add-Content -Path $HawAffFile
+
+
 }
 finally
 {
